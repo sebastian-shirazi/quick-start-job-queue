e44c081735ec98772f4c5514c9e6bc7745054ca9
sebastian-shirazi <byr5uk@virginia.edu>
2024-05-23 12:19:27 -0400

    Wrote tests for enqueue and dequeue

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,24 @@
-from project import main
+from project.main import Queue
 
 
-def test_enqueue():
-    q = main.Queue()
+def test_enqueue_default():
+    q = Queue()
+    q = Queue()
     q.enqueue("123", "task")
     assert q.queue == [("123", "task", 10)]
+
+def test_enqueue_priority():
+    q = Queue()
+    q.enqueue("123", "task", 5)
+    assert q.queue == [("123", "task", 5)]
+
+def test_dequeue():
+    q = Queue()
+    q.queue = [("123", "task", 5), ("456", "task2", 10)]
+    assert ("123", "task", 5) == q.dequeue()
+    assert q.queue == [("456", "task2", 10)]
+
+def test_dequeue_empty():
+    q = Queue()
+    assert q.dequeue() == None
+
