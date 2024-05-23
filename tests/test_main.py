6abda373a830b43bac0d44991556ed07e11dc83b
sebastian-shirazi <byr5uk@virginia.edu>
2024-05-23 17:43:15 -0400

    Wrote more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,48 @@
 from project.main import Queue
 
 
-def test_enqueue_default():
-    q = Queue()
+def test_can_enqueue_a_job_defualt_priority():
     q = Queue()
     q.enqueue("123", "task")
     assert q.queue == [("123", "task", 10)]
 
-def test_enqueue_priority():
+def test_can_enqueue_a_job():
     q = Queue()
     q.enqueue("123", "task", 5)
     assert q.queue == [("123", "task", 5)]
 
-def test_dequeue():
+def test_can_dequeue_a_queued_job():
     q = Queue()
     q.queue = [("123", "task", 5), ("456", "task2", 10)]
     assert ("123", "task", 5) == q.dequeue()
     assert q.queue == [("456", "task2", 10)]
 
-def test_dequeue_empty():
+def test_priority_jobs_are_dequeued_first():
+    pass
+
+def test_dequeue_on_empty_queue_returns_none():
     q = Queue()
     assert q.dequeue() == None
 
+def test_can_list_jobs_in_queue():
+    q = Queue()
+    q.queue = [("123", "task", 5), ("456", "task2", 10)]
+    assert q.get_queue() == print(f"Job 1: {q.queue[0]}\nJob 2: {q.queue[1]}")
+
+def test_can_read_jobs_from_file():
+    q = Queue()
+    q.read_file("Sample.txt")
+    q.priotity_sort()
+    assert q.queue == [('5cb139e2-2344-4ede-aa4c-4ec1e940ab28', 'Task 2', 1),
+        ('8b0f47f2-a833-4f12-bc9e-096e49c015ad', 'Task 3', 2),
+        ('c8fd914b-8339-4ea0-907a-dc685a8f4758', 'Task 5', 2),
+        ('b0293ea7-f8bc-4c3a-af5e-9b1d858ed18a', 'Task 1', 3),
+        ('eda49cfe-c2f5-49f7-917d-ffc3d3111e63', 'Task 4', 3)
+    ]
+
+def test_throws_error_when_file_not_found():
+    q = Queue()
+    try:
+        q.read_file("Sample.csv")
+    except FileNotFoundError as err:
+        assert str(err) == "Invalid file format - file not found or not a .txt file"
